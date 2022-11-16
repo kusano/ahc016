413bb864d01094f2e4216eefa08924dd20a731dc
kusano <kusano@users.noreply.github.com>
2022-11-16 19:29:05 +0900

    常に0を出力
    453607

---
+++ A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int M;
+    double e;
+    cin>>M>>e;
+
+    cout<<4<<endl;
+    for (int i=0; i<100; i++)
+        cout<<"000000"<<endl;
+
+    for (int k=0; k<100; k++)
+    {
+        string g;
+        cin>>g;
+        cout<<0<<endl;
+    }
+}
