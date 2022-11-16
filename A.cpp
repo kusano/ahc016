d9c9a73bfa84cb8536657b9cf160c14437761bf9
kusano <kusano@users.noreply.github.com>
2022-11-17 04:14:06 +0900

    辺の本数にエンコード2
    148501755

--- A.cpp
+++ A.cpp
@@ -71,6 +71,84 @@ public:
     }
 };
 
+// 辺の本数にエンコード（N=100の辺を全て使う）
+class TranscoderEdgeNum100: public Transcoder
+{
+    int M = 0;
+    double e = 0.;
+    int N = 0;
+    int w = 0;
+    vector<double> F;
+
+    double comb(int a, int b)
+    {
+        return F[a]-F[b]-F[a-b];
+    }
+
+public:
+    vector<vector<vector<int>>> init(int M, double e)
+    {
+        this->M = M;
+        this->e = e;
+        N = 100;
+
+        w = (N*(N-1)/2-1)/(M-1);
+
+        vector<vector<vector<int>>> G(M, vector<vector<int>>(N, vector<int>(N)));
+        for (int i=0; i<M; i++)
+        {
+            int n = i*w;
+            int c = 0;
+            for (int j=0; j<N && c<n; j++)
+                for (int k=0; k<j && c<n; k++)
+                {
+                    G[i][j][k] = G[i][k][j] = 1;
+                    c++;
+                }
+        }
+
+        F = vector<double>(N*N/2);
+        F[0] = 0.;
+        for (int i=1; i<N*N/2; i++)
+            F[i] = log(i)+F[i-1];
+
+        return G;
+    }
+
+    int decode(vector<vector<int>> H)
+    {
+        int c = 0;
+        for (int i=0; i<N; i++)
+            for (int j=0; j<i; j++)
+                c += H[i][j];
+
+        double maxp = -1.;
+        int maxi = 0;
+        for (int i=0; i<M; i++)
+        {
+            int c0 = i*w;
+            // c0 -> c となる確率
+            double p = 0.;
+            for (int j=0; j<=c0; j++)
+            {
+                int j2 = c-(c0-j);
+                if (0<=j2 && j2<=N*(N-1)/2-c0)
+                    p += exp(
+                        comb(c0, j)+log(e)*j+log(1.-e)*(c0-j)+
+                        comb(N*(N-1)/2-c0, j2)+log(e)*j2+log(1.-e)*(N*(N-1)/2-c0-j2));
+            }
+
+            if (p>maxp)
+            {
+                maxp = p;
+                maxi = i;
+            }
+        }
+
+        return maxi;
+    }
+};
+
 void test(Transcoder *transcoder)
 {
     const int T = 50;
@@ -130,6 +208,8 @@ void test(Transcoder *transcoder)
             int ans = transcoder->decode(H2);
             if (ans!=s)
                 E++;
+
+            //printf("s=%d, ans=%d\n", s, ans);
         }
 
         long long score = (long long)(1e9*pow(0.9, E)/N+.5);
@@ -144,8 +224,9 @@ void test(Transcoder *transcoder)
 int main()
 {
     //TranscoderFixed fixed;
-    TranscoderEdgeNum edge;
-    Transcoder *transcoder = &edge;
+    //TranscoderEdgeNum edge;
+    TranscoderEdgeNum100 edge100;
+    Transcoder *transcoder = &edge100;
 
 #ifdef WIN32
     test(transcoder);
